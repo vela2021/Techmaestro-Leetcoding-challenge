84f102d0662f22ebabfd8c56c7a8cdbad8eafb00
vela2021 <102014752+vela2021@users.noreply.github.com>
2022-03-24 11:55:42 +0530

    Time: 3 ms (60.88%), Space: 8.7 MB (62.57%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]=digits[i]+1;
+                return digits;
+            }else if (digits[i]==9)
+            {
+                digits[i]=0;
+            }
+        }digits.insert(digits.begin(),1);
+    return digits;
+    }
+};
