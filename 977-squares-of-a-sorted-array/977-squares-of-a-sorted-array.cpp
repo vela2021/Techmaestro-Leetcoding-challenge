a74f991019a8d220ac14d39720b057a92aedb318
vela2021 <102014752+vela2021@users.noreply.github.com>
2022-03-25 10:02:59 +0530

    Time: 44 ms (58.51%), Space: 26 MB (55.24%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+public:// TWO POINTER METHOD:
     vector<int> sortedSquares(vector<int>& nums) {
     int start=0,end=nums.size()-1,i=end,j;vector <int>answer(end+1,0);
         while(start<=end)
